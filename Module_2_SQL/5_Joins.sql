8a6dbdd8da4db5b5cf1ba3d924521cd37cfbfd4f
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-05-04 20:10:11 +0530

    joins

--- Module_2_SQL/5_Joins.sql
+++ Module_2_SQL/5_Joins.sql
@@ -37,3 +37,76 @@ FROM department
 INNER JOIN employee ON 
 -- on which column you need comparision
 employee.DEPTCODE = department.DEPTCODE;
+
+--INNERJoin
+-- select everything from employee
+
+-- INNER JOIN to get all the results
+SELECT * FROM employee
+-- and while selecting from employee make a inner join with department
+INNER JOIN DEPARTMENT
+ON employee.DEPTCODE=DEPARTMENT.DEPTCODE;
+
+SELECT employee.DEPTCODE, 
+department.DEPTCODE as depcode , 
+employee.email, 
+department.LOCATION
+FROM department -- department columns will get first because it is written first
+INNER JOIN employee -- employee column will come second because it is written second or after INNERJOIN
+-- 
+ON DEPARTMENT.DEPTCODE = employee.DEPTCODE;
+
+-- to get your result in a given order
+
+SELECT employee.DEPTCODE as eDEPTCODE, 
+employee.EmpFName,
+department.LOCATION,
+department.DeptName
+FROM department
+-- and while selecting from employee make a inner join with department
+INNER JOIN employee
+-- on which column we need to make intersection
+-- on intersection corresponding rows of both the table will be joined
+ON DEPARTMENT.DEPTCODE=employee.DEPTCODE;
+
+-- list of employees who belong to a valid particular department
+
+-- LEFT 
+SELECT employee.DEPTCODE as EDEPTCODE, 
+department.DEPTCODE ,
+employee.EmpFName, 
+department.LOCATION
+FROM employee
+LEFT JOIN department
+ON  employee.DEPTCODE= DEPARTMENT.DEPTCODE;
+
+--RIGHT
+
+SELECT employee.DEPTCODE as EDEPTCODE, 
+department.DEPTCODE ,
+employee.EmpFName, 
+department.LOCATION
+FROM employee
+RIGHT JOIN department
+ON  employee.DEPTCODE= DEPARTMENT.DEPTCODE;
+
+-- FULL OUTER JOIN
+
+ 
+SELECT employee.DEPTCODE as EDEPTCODE, 
+department.DEPTCODE ,
+employee.EmpFName, 
+department.LOCATION
+FROM employee
+LEFT JOIN department
+ON  employee.DEPTCODE= DEPARTMENT.DEPTCODE;
+
+UNION
+
+SELECT employee.DEPTCODE as EDEPTCODE, 
+department.DEPTCODE ,
+employee.EmpFName, 
+department.LOCATION
+FROM employee
+RIGHT JOIN department
+ON  employee.DEPTCODE= DEPARTMENT.DEPTCODE;
