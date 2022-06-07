91366368696e3755ae0aeac86e8df89087f53bd4
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-06-07 19:38:28 +0530

    js

---
+++ Module_1/L3_App_with_JS/fs.js
@@ -0,0 +1,10 @@
+// file system
+
+// folder and files deal
+
+
+let fs = requre("fs");
+console.log("before");
+
+fs.readFileSync();
+console.log("After");
---
+++ Module_1/L3_App_with_JS/os.js
@@ -0,0 +1,18 @@
+// nodejs please give me the access of file system module(app)
+
+let os = require("os");
+
+// which os is installed
+console.log("--------------------------");
+console.log(os.arch());
+
+// give data about your cpus
+console.log("--------------------------");
+console.log(os.cpus());
+
+// wifi info
+console.log("--------------------------");
+console.log(os.networkInterfaces());
+
+// reference
+
